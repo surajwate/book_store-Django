bc73fff607d3735e2b2b25a4d798a5d409256d38
Suraj Wate <suraj.wate@gmail.com>
2023-07-05 10:49:03 +0100

    Added relation
    Added a one-to-many Relation & Migrations

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,10 +5,14 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True) 
 
